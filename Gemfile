a56fdacfa08f5712b760b6a84963b8558bd3655c
Gino Garcia <ginogarcia@netzero.net>
2012-04-15 17:30:48 -0500

    initial commit

---
+++ Gemfile
@@ -0,0 +1,26 @@
+source 'http://rubygems.org'
+
+gem 'rails', '3.0.4'
+
+# Bundle edge Rails instead:
+# gem 'rails', :git => 'git://github.com/rails/rails.git'
+
+gem 'sqlite3'
+gem 'gravatar_image_tag', '1.0.0.pre2'
+gem 'will_paginate', '3.0.pre2'
+
+group :development do
+  gem 'rspec-rails', '2.6.1'
+  gem 'annotate', '2.4.0'
+  gem 'faker', '0.3.1'
+  gem 'annotate', '2.4.0'
+ 
+end
+
+group :test do
+  gem 'rspec-rails', '2.6.1'
+  gem 'webrat', '0.7.1'
+   gem 'factory_girl_rails', '1.0'
+  
+end
+
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Lab11::Application.config.secret_token = 'e54458a339e7224df46eca23ad61b46e7a98a72f3d876226c6e29246dd04f10dd9e91bb87420eec1c5bd4cfa7aabf1527b8ec65baa625d040e626f043ab4df23'
